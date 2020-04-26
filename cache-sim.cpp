dbe025b43e41384da1e6be5b679c7b842a66339b
Kyle Stigler <kstigle1@binghamton.edu>
2020-04-26 16:31:01 -0400

    formatting and variables

--- cache-sim.cpp
+++ cache-sim.cpp
@@ -319,120 +319,150 @@ void setPrefetching(string fileName, ofstream& outFile, int way)
 {
 	ifstream inFile(fileName);
 
-    int sets = 512 / way;
-    int cache[sets][way];
-    int recent[sets][toPass];
-    for (int i = 0; i < sets; i++) {
-        for (int j = 0; j < way; j++) { 
-            cache[i][j] = -1;
-            recent[i][j] = -1;
-        }
-    }
+    	int sets = 512/way;
+    	int cache[sets][way];
+    	int recent[sets][toPass];
+    	for (int i=0; i<sets; i++) 
+	{
+        	for (int j=0; j<way; j++) 
+		{ 
+            		cache[i][j] = -1;
+            		recent[i][j] = -1;
+        	}
+    	}
 
-    string str;
-    int hit = 0, total = 0;
-    int log_size = log2(sets);
-    int index, next_index, full_addr, addr, tag, next_tag;
+    	string ldstr;
+    	int hit = 0; 
+	int total = 0;
+    	int logSize = log2(sets);
+   	int index;
+	int nIndex;
+	int addrOff;
+	int addr; 
+	int tag; 
+	int nTag;
 
-    while (inFile >> str >> hex >> full_addr) {
-        addr = full_addr >> 5;
-        index = addr % sets;
-        tag = addr >> log_size;
-        int in_cache = 0;
-        int next_in_cache = 0;
-        int lru_ret = -1;
+    	while (inFile >> ldstr >> hex >> addrOff) 
+	{
+		total++;
 
-        next_index = (addr + 1) % sets;
-        next_tag = (full_addr + 32) >> (log_size + 5);
-        for (int k = 0; k < way; k++) {
-            if (cache[index][k] == tag) {
-                hit++;
-                lru_ret = LRU(index, way, k, recent);
-                in_cache = 1;
-                break;
-            }
-        }
-        for (int k = 0; k < way; k++) {
-            if (cache[next_index][k] == next_tag) {
-                lru_ret = LRU(next_index, way, k, recent);
-                next_in_cache = 1;
-                break;
-            }
-        }
-        if (in_cache == 0) {
-            lru_ret = LRU(index, way, -1, recent);
-            cache[index][lru_ret] = tag;
-        }
-        if (next_in_cache == 0) {
-            lru_ret = LRU(next_index, way, -1, recent);
-            cache[next_index][lru_ret] = next_tag;
-        }
+        	addr = addrOff>>5;
+        	index = addr%sets;
+        	tag = addr>>logSize;
+        	int goodHit = 0;
+        	int nGoodHit = 0;
+       	 	int LRUret = -1;
 
-        total++;
-    }
+        	nIndex = (addr+1)%sets;
+        	nTag = (addrOff+32)>>(logSize+5);
+        	for (int k=0; k<way; k++) 
+		{
+            		if (cache[index][k] == tag) 
+			{
+                		hit++;
+                		LRUret = LRU(index, way, k, recent);
+                		goodHit = 1;
+                		break;
+            		}
+        	}
+        	for (int k=0; k<way; k++) 
+		{
+            		if (cache[nIndex][k] == nTag) 
+			{
+                		LRUret = LRU(nIndex, way, k, recent);
+                		nGoodHit = 1;
+                		break;
+            		}	
+        	}
+        	if (goodHit == 0) 
+		{
+            		LRUret = LRU(index, way, -1, recent);
+            		cache[index][LRUret] = tag;
+        	}
+        	if (nGoodHit == 0) 
+		{
+            		LRUret = LRU(nIndex, way, -1, recent);
+            		cache[nIndex][LRUret] = nTag;
+        	}
+    	}
 
-    outFile << hit << "," << total;
+    	outFile << hit << "," << total;
 }
 
 void setPrefetchingMiss(string fileName, ofstream& outFile, int way)
 {
 	ifstream inFile(fileName);
 
-    int sets = 512 / way;
-    int cache[sets][way];
-    int recent[sets][toPass];
-    for (int i = 0; i < sets; i++) {
-        for (int j = 0; j < way; j++) { 
-            cache[i][j] = -1;
-            recent[i][j] = -1;
-        }
-    }
+    	int sets = 512/way;
+    	int cache[sets][way];
+    	int recent[sets][toPass];
+    	for (int i=0; i<sets; i++) 
+	{
+        	for (int j=0; j<way; j++) 
+		{ 
+            		cache[i][j] = -1;
+            		recent[i][j] = -1;
+        	}
+    	}
 
-    string str;
-    int hit = 0, total = 0;
-    int log_size = log2(sets);
-    int index, next_index, full_addr, addr, tag, next_tag;
+    	string ldstr;
+    	int hit = 0;
+	int total = 0;
+    	int logSize = log2(sets);
+    	int index;
+	int nIndex;
+	int addrOff;
+	int addr;
+	int tag;
+	int nTag;
 
-    while (inFile >> str >> hex >> full_addr) {
-        addr = full_addr >> 5;
-        index = addr % sets;
-        tag = addr >> log_size;
-        int in_cache = 0;
-        int next_in_cache = 0;
-        int lru_ret = -1;
+    	while (inFile >> ldstr >> hex >> addrOff) 
+	{
+		total++;
 
-        next_index = (addr + 1) % sets;
-        next_tag = (full_addr + 32) >> (log_size + 5);
-        for (int k = 0; k < way; k++) {
-            if (cache[index][k] == tag) {
-                hit++;
-                lru_ret = LRU(index, way, k, recent);
-                in_cache = 1;
-                break;
-            }
-        }
-        if (in_cache == 0) {
-            lru_ret = LRU(index, way, -1, recent);
-            cache[index][lru_ret] = tag;
+        	addr = addrOff>>5;
+        	index = addr%sets;
+        	tag = addr>>logSize;
+        	int goodHit = 0;
+        	int nGoodHit = 0;
+        	int LRUret = -1;
 
-            for (int k = 0; k < way; k++) {
-                if (cache[next_index][k] == next_tag) {
-                    lru_ret = LRU(next_index, way, k, recent);
-                    next_in_cache = 1;
-                    break;
-                }
-            }
+        	nIndex = (addr+1)%sets;
+        	nTag = (addrOff+32)>>(logSize+5);
+        	for (int k=0; k<way; k++) 
+		{
+            		if (cache[index][k] == tag) 
+			{
+                		hit++;
+                		LRUret = LRU(index, way, k, recent);
+                		goodHit = 1;
+                		break;
+            		}
+        	}
+        	if (goodHit == 0) 
+		{
+            		LRUret = LRU(index, way, -1, recent);
+            		cache[index][LRUret] = tag;
 
-            if (next_in_cache == 0) {
-                lru_ret = LRU(next_index, way, -1, recent);
-                cache[next_index][lru_ret] = next_tag;
-            }
-        }
+            		for (int k=0; k<way; k++) 
+			{
+                		if (cache[nIndex][k] == nTag) 
+				{
+                    			LRUret = LRU(nIndex, way, k, recent);
+                    			nGoodHit = 1;
+                    			break;
+                		}
+            		}
 
-        total++;
-    }
+            		if (nGoodHit == 0) 
+			{
+                		LRUret = LRU(nIndex, way, -1, recent);
+                		cache[nIndex][LRUret] = nTag;
+            		}
+        	}
+    	}	
 
-    outFile << hit << "," << total;
+    	outFile << hit << "," << total;
 }
 
 
